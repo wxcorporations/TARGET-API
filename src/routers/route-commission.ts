08e364058033b3a2d38e9e5c05feddd1567da8d6
fernando j <fer.j.doc@gmail.com>
2025-11-25 05:41:54 -0300

    refactory: na rota commission

--- src/routers/route-commission.ts
+++ src/routers/route-commission.ts
@@ -1,15 +1,12 @@
 import { Router } from 'express'
 
-import { list, filter, register, update } from '../controllers/commission-controller'
+import { list } from '../controllers/commission-controller'
 
 const route = Router()
 
 route.get('/commissions', list)
 
-route.get('/commissions', filter)
+// route.get('/commissions', filter)
 
-route.get('/commissions', register)
-
-route.get('/commissions', update)
 
 export default route
--- src/routers/route-sales.ts
+++ src/routers/route-sales.ts
@@ -1,15 +1,10 @@
 import { Router } from 'express'
 
-import { list, filter, register, update } from '../controllers/sales-controller'
+import { list } from '../controllers/sales-controller'
 
 const route = Router()
 
 route.get('/sales', list)
 
-route.get('/sales', filter)
-
-route.post('/sale', register)
-
-route.put('/sale', update)
 
 export default route
